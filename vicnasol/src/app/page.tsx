22f022ecc1dfc4c0b96b6f67c3f423f1ab5c9df4
Giancarlo <giancarlozanarini9@gmail.com>
2024-06-10 18:57:43 -0300

    Arrancado el proyecto creando principios de pagina main

---
+++ vicnasol/src/app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <main className="h-screen bg-green-50">
+      <div className="">
+        <h1 className="text-3xl font-bold text-center pt-4">En proceso...</h1>
+      </div>
+    </main>
+  );
+}
